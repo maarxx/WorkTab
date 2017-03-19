d5a93b7451dfa7486216a26781ca8b9fa305a553
maarxx <chris.lewark@gmail.com>
2017-03-19 13:31:31 -0400

    Leveraged addIfNotContains

--- Source/Fluffy_Tabs/Work/NameTagProcessor.cs
+++ Source/Fluffy_Tabs/Work/NameTagProcessor.cs
@@ -224,7 +224,8 @@ namespace Fluffy_Tabs
         {
             foreach (WorkGiverDef wgd in group)
             {
-                final.Add(wgd);
+                //final.Add(wgd);
+                addIfNotContains(wgd, ref final);
             }
         }
 
@@ -288,36 +289,20 @@ namespace Fluffy_Tabs
                     output.Add(MyMapper.s("smith,115"));
                     output.Add(MyMapper.s("work,75"));
                     output.Add(MyMapper.s("produce components,50"));
-                    output.Add(MyMapper.s("tailor,110"));
                     output.Add(MyMapper.s("craft,100"));
                     output.Add(MyMapper.s("refine chemicals,97"));
                     output.Add(MyMapper.s("produce drugs,95"));
+                    output.Add(MyMapper.s("tailor,110"));
                     //output.Add(MyMapper.s("stonecut,90"));
                     //output.Add(MyMapper.s("refine,80"));
                     break;
                 case 'P':
                 case 'p':
                     output.Add(MyMapper.s("produce drugs,95"));
-                    output.Add(MyMapper.s("tailor,110"));
-                    output.Add(MyMapper.s("smith,115"));
-                    output.Add(MyMapper.s("work,75"));
-                    output.Add(MyMapper.s("produce components,50"));
-                    output.Add(MyMapper.s("craft,100"));
-                    output.Add(MyMapper.s("refine chemicals,97"));
-                    //output.Add(MyMapper.s("stonecut,90"));
-                    //output.Add(MyMapper.s("refine,80"));
                     break;
                 case 'T':
                 case 't':
                     output.Add(MyMapper.s("tailor,110"));
-                    output.Add(MyMapper.s("produce drugs,95"));
-                    output.Add(MyMapper.s("smith,115"));
-                    output.Add(MyMapper.s("work,75"));
-                    output.Add(MyMapper.s("produce components,50"));
-                    output.Add(MyMapper.s("craft,100"));
-                    output.Add(MyMapper.s("refine chemicals,97"));
-                    //output.Add(MyMapper.s("stonecut,90"));
-                    //output.Add(MyMapper.s("refine,80"));
                     break;
                 case 'A':
                 case 'a':
